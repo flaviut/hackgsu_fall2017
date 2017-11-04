72657e526690a4d08d787aa1cbb18fa17c663564
Flaviu Tamas <tamasflaviu@gmail.com>
2017-11-04 13:29:47 -0400

    Fix arduino code

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -3,15 +3,17 @@
 
 
 BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
-BLEService doorService("9B9B");
+BLEService doorService("9c856326-4517-407e-a661-faba175acdfe");
 
-BLECharacteristic doorChar("5BF9",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify, 16);  // remote clients will be able to get notifications if this characteristic changes
-                              // the characteristic is 2 bytes long as the first field needs to be "Flags" as per BLE specifications
-                              // https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+BLECharacteristic doorChar(
+  "6f9af486-ee00-43ac-8df6-897e05af1aa6",  // standard 16-bit characteristic UUID
+  BLERead | BLENotify, 17);  // remote clients will be able to get notifications if this characteristic changes
+                             // the characteristic is 2 bytes long as the first field needs to be "Flags" as per BLE specifications
+                             // https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
 
 void setup()
 {
+  pinMode(13, OUTPUT);
   Serial.begin(115000);
   CurieIMU.begin();
   blePeripheral.setLocalName("DoorSensor");
@@ -22,8 +24,8 @@ void setup()
   /* Now activate the BLE device.  It will start continuously transmitting BLE
      advertising packets and will be visible to remote BLE central devices
      until it receives a new connection */
+  Serial.println("Beginning work");
   blePeripheral.begin();
-
 }
 
 void write_int_to_array(uint32_t v, unsigned char target[]) {
@@ -32,11 +34,40 @@ void write_int_to_array(uint32_t v, unsigned char target[]) {
   target[2] = v >>  8 & 0xFF;
   target[3] = v >>  0 & 0xFF;
 }
-void loop()
-{
+
+int oldHeartRate = 0;  // last heart rate reading from analog input
+long previousMillis = 0;  // last time the heart rate was checked, in ms
+
+void loop() {
+  // listen for BLE peripherals to connect:
   BLECentral central = blePeripheral.central();
-  
-  while (central && central.connected()) {
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(13, HIGH);
+
+    // check the heart rate measurement every 200ms
+    // as long as the central is still connected:
+    while (central.connected()) {
+      long currentMillis = millis();
+      // if 200ms have passed, check the heart rate measurement:
+      if (currentMillis - previousMillis >= 50) {
+        previousMillis = currentMillis;
+        updateSensors();
+      }
+    }
+    // when the central disconnects, turn off the LED:
+    digitalWrite(13, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void updateSensors() {
     int x, y, z;
     unsigned char output[17];
     memset(output, 0, sizeof(output));
@@ -49,7 +80,5 @@ void loop()
     write_int_to_array((uint32_t)y, &output[9]);
     write_int_to_array((uint32_t)z, &output[13]);
     doorChar.setValue(output, 17);
-    delay(50);
-  }
 }
 
