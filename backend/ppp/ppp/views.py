b5697d097c8ae17f4456e06ffd5f2d39da5fa304
jpham197 <jpham197@gmail.com>
2017-11-04 16:05:26 -0400

    Automated ID increment and show some data

--- backend/ppp/ppp/views.py
+++ backend/ppp/ppp/views.py
@@ -3,13 +3,6 @@ from ppp import app
 import json
 import sqlite3 as sql
 
-s = "test"
-
-#studnet object with parsed JSON
-student = {"101":{"class":'V', "Name":'Rohit',  "Roll_no":7},
-           "102":{"class":'V', "Name":'David',  "Roll_no":8},
-           "103":{"class":'V', "Name":'Samiya', "Roll_no":12}}
-
 #json thing
 myToilets = {"toilets":[{"id": "1", "toiletId": "1", "timestamp": "...", "action": "closed"},
                       {"id": "2", "toiletId": "2", "timestamp": "...", "action": "closed"},
@@ -18,32 +11,40 @@ myToilets = {"toilets":[{"id": "1", "toiletId": "1", "timestamp": "...", "action
                       {"id": "5", "toiletId": "5", "timestamp": "...", "action": "closed"},
                       {"id": "6", "toiletId": "6", "timestamp": "...", "action": "open"},
                         {"id": "7", "toiletId": "7", "timestamp": "...", "action": "closed"}]}
+
 @app.route('/what')
 def wha_t():
-    x = 0
+    a = 0
     for num in myToilets['toilets']:
-        x += 1
-    return str(x)
+        a += 1
+    return str(a)
 @app.route('/database')
 def toilet_info():
     con = sql.connect('test.db')
     with con:
         c = con.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS Toilets(id INTEGER PRIMARY KEY,toiletId INT, timestamp TEXT, action TEXT)")
-        for num in myToilets['toilets']:
-            c.execute('INSERT INTO Toilets (timestamp, action) VALUES (?,?)', (num['timestamp'], num['action']))
+        c.execute("CREATE TABLE IF NOT EXISTS Toilets(id INTEGER PRIMARY KEY,toiletId INTEGER, timestamp TEXT, action TEXT)")
+        # c.execute('DROP TABLE IF EXISTS Toilets')
     return "Success"
 @app.route('/showdbcolumn')
 def show_db_column():
-	con = sql.connect('test.db')
-	con.row_factory = sql.Row
-	cur = con.cursor()
-	cur.execute('SELECT action FROM Toilets')
-	rows = cur.fetchall()
-	list = []
-	for row in rows:
-		list.append(tuple(row))
-	return json.dumps(list)
+    con = sql.connect('test.db')
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute('SELECT id FROM Toilets')
+    rows = cur.fetchall()
+    list = []
+    for row in rows:
+        list.append(tuple(row))
+    return json.dumps(list)
+@app.route('/addinfo')
+def add_info():
+    con = sql.connect('test.db')
+    with con:
+        c = con.cursor()
+        for num in myToilets['toilets']:
+            c.execute('INSERT INTO Toilets (timestamp, action) VALUES (?,?)', (num['timestamp'], num['action']))
+    return "Data inserted"
 @app.route('/json')
 def index():
-   return json.dumps(student)
+   return json.dumps(myToilets)
