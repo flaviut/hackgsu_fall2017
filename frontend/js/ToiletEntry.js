8a73ee51671f7ad4d483d312744f1b19b7cabb1d
Flaviu Tamas <tamasflaviu@gmail.com>
2017-11-05 10:39:57 -0500

    dagjsaoihjre

--- frontend/js/ToiletEntry.js
+++ frontend/js/ToiletEntry.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const ToiletEntry = (props) => {
   return (
     <tr>
-      <td>Toilet {props.toiletId}</td><td>{props.status == "clean" ? "👌" : "☣"}</td>
+      <td>Toilet {props.toiletId}</td><td>{props.status == "clean" ? "Clean" : <b>Needs Attention</b>}</td>
     </tr>);
 };
 
